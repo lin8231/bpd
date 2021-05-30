632c9b6ca1b604ab5fcbf9f3107a49c8aa63a74a
lin8231 <62003343+lin8231@users.noreply.github.com>
2021-05-30 21:03:09 +1200

    make minor changes to colours''

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
 console.log("scripts");
 
-// Get the modal
-var modal = document.getElementById("modal-bg");
+const btn = document.getElementById("trigger-modal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("trigger-modal");
-var additionalBtns = document.getElementsByClassName("sign-up");
+const span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("modal-bg");
+const cancel = document.getElementById("form-cancel");
+const submit = document.getElementById("form-submit");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
+btn.onclick = () => {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span.onclick = () => {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+cancel.onclick = () => {
+  modal.style.display = "none";
+};
+
+submit.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
-
-console.log("scripts");
